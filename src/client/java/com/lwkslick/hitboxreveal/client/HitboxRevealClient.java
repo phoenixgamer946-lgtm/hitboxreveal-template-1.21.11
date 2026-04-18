a576806dc5e31f0e14e801b0edb507ac882799b8
lwkSlick <phoenixgamer946@gmail.com>
2026-04-18 11:25:53 +0530

    Initial commit

---
+++ src/client/java/com/lwkslick/hitboxreveal/client/HitboxRevealClient.java
@@ -0,0 +1,10 @@
+package com.lwkslick.hitboxreveal.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HitboxRevealClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
